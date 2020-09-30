446352f16268a90d1b781f530020041c7c5934e6
Opilane TTHK <opilane@tthk.ee>
2020-09-30 10:43:33 +0300

    Add project files.

---
+++ SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af4e3fbf-c49d-4472-ab4f-d3622ab17462")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SportsStore.WebUI/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+
+namespace SportsStore.WebUI.App_Start
+{
+    private static void RegisterServices(IKernel kernel)
+    {
+        System.Web.Mvc.DependencyResolver.SetResolver(new
+        SportsStore.WebUI.Infrastructure.NinjectDependencyResolver(kernel));
+    }
+
+}
